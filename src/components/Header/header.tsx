13b7facdf4ef6ec1b323aaf2050da877c6ab8729
dev2842000 <dev69440@gmail.com>
2024-06-01 18:41:19 +0530

    working on routing

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -15,6 +15,9 @@ const Header: React.FC = () => {
         <li className="nav-item">
           <Link to="/profile" className="nav-link">Profile</Link>
         </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">Login</Link>
+        </li>
       </ul>
     </nav>
   );
